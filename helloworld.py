b806e6f71157aeb99d0fdb5b727fdf9edd1de636
ccppoo <shmishmi79@gmail.com>
2022-05-27 14:10:52 +0900

    update helloworld.py
    print your name!

--- helloworld.py
+++ helloworld.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 
-def hello():
-  print(f'hello world!')
+def hello(name : str) -> None:
+  print(f'hello {name}!')
   
   
-def main():
-  hello()
+def main(): 
+  NAME = 'Your name'
+  hello(NAME)
   
 if __name__ == '__main__':
   main()
