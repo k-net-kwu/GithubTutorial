a5e106b16057a06a1d9bbeeb05686dd59b2aaa8b
ccppoo <shmishmi79@gmail.com>
2022-05-27 14:57:33 +0900

    add bye function
    bye bye

--- helloworld.py
+++ helloworld.py
@@ -2,11 +2,14 @@
 
 def hello(name : str) -> None:
   print(f'hello {name}!')
-  
+
+def bye(name : str) -> None:
+  print(f'bye {name}!') 
   
 def main(): 
   NAME = 'Your name'
   hello(NAME)
-  
+  bye(NAME)
+
 if __name__ == '__main__':
   main()
